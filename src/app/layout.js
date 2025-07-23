2dc5a32bbfe72b18980ac808e27dd2acb9ae2461
Najlaa Loutfi <93660922+sleepyNajlio@users.noreply.github.com>
2025-07-23 03:44:19 +0100

    GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-19SSLE284Z"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-19SSLE284Z');
+        </script>
         {/* <link rel="preload" as="image" href="/images/hero-image.avif" type="image/avif" /> */}
       </head>
       <body className={`${playfair.variable} font-work-sans bg-background `}>
